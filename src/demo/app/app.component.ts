57e759528e4bf830f810fbca1c694fb2ac00dff9
saurabh <saurabhsingh368@gmail.com>
2018-09-18 03:04:05 +0530

    modified JSON in demo to show earlier fixed cases

--- src/demo/app/app.component.ts
+++ src/demo/app/app.component.ts
@@ -5,25 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  result = {
-    "content": "",
-    "inList": false,
-    "isHeading": false,
-    "nodeID": "s1519994311309kjsbjkdnlsdnflkdsnflksdnflkdsnflkdsfnlksdfnlkfnlklkdfndfnslkdfnlkdfnlkdfnlkfnlkf",
-    "cool" : [
-      {
-      "a": 1,
-      "b": 1
-    },
-    {
-      "a": 1,
-      "b": 1
-    }
-  ],
-  "obj": {
-    "o": "o2"
-  }
-}
+    result = {
+        'content': '',
+        'inList': false,
+        'isHeading': false,
+        'nodeID': 's1519994311309kjsbjkdnlsdnflkdsnflksdnflkdsnflkdsfnlksdfnlkfnlklkdfndfnslkdfnlkdfnlkdfnlkfnlkf',
+        'cool' : [
+            {
+                'a': 1,
+                'b': 1
+            },
+            {
+                'a': 1,
+                'b': 1
+            }
+        ],
+        'obj': {
+            'o': 'o2',
+            'o1': {k1: ['val1', 123, {'a': 'b'}]},
+            'o2': {a: [true, 'two', 3]}
+        }
+    };
   constructor() {
   }
 }
